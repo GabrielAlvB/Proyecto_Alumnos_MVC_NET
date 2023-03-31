a7b041583b26904b1d35fc0455750189f60379eb
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-03-31 09:05:10 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Alumnos_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Alumnos_MVC.MvcApplication" Language="C#" %>
---
+++ Proyecto_Alumnos_MVC/Models/Alumnos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_Alumnos_MVC.Models
+{
+    public class Alumnos
+    {
+
+
+
+    }
+}
