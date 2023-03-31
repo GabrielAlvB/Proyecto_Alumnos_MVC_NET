b06c4c6b579b2ce7d6ea2286d613e5365c084239
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-03-31 09:50:35 -0600

    Se agrego KEY en la clase Materias, de otra forma no se permite crear el controlador para la clase Materias

--- Proyecto_Alumnos_MVC/Models/Materias.cs
+++ Proyecto_Alumnos_MVC/Models/Materias.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyecto_Alumnos_MVC.Models
 {
     public class Materias
     {
+        [Key]
+        public int ID { get; set; }
         public string Name { get; set; }
         //el boolean del estado no estoy seguro como lo tome para dar las altas
         public Boolean Estado { get; set; }
