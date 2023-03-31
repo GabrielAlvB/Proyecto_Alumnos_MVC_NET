512816e8806c4be1a7c3429b4bc6e98d27325784
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-03-31 09:37:02 -0600

    Se creo la clase materias en el modelo, además de agregar comentarios a las secciones

--- Proyecto_Alumnos_MVC/Models/Alumnos.cs
+++ Proyecto_Alumnos_MVC/Models/Alumnos.cs
@@ -14,6 +14,12 @@ namespace Proyecto_Alumnos_MVC.Models
         public string Name { get; set; }
         public int Age { get; set; }
         public int Grado { get; set; }
+        /*
+        public Boolean Estado { get; set; }
+        si se quisiera agregar un parametro se tendria que actualizar la base de datos 
+        ademas de hacer el proceso denuevo de las vistas para que se modificara
+
+         */
         }
 
     public class AlumnosDBContext : DbContext
---
+++ Proyecto_Alumnos_MVC/Models/Materias.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Proyecto_Alumnos_MVC.Models
+{
+    public class Materias
+    {
+        public string Name { get; set; }
+        //el boolean del estado no estoy seguro como lo tome para dar las altas
+        public Boolean Estado { get; set; }
+
+    }
+
+    public class MateriasDBContext : DbContext
+    {
+        public DbSet<Materias> Materia { get; set; }
+    }
+
+
+}
