ef485c8c87879395ef7df767973cced4acd104d5
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-03-31 09:12:13 -0600

    Se genero la clase en el  modelo, la clase se llama alumnos

--- Proyecto_Alumnos_MVC/Models/Alumnos.cs
+++ Proyecto_Alumnos_MVC/Models/Alumnos.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+//usando fremework entity para la base de datos
+using System.Data.Entity;
+
 namespace Proyecto_Alumnos_MVC.Models
 {
     public class Alumnos
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Grado { get; set; }
+        }
 
-
-
+    public class AlumnosDBContext : DbContext
+    {
+        public DbSet<Alumnos> Alumno { get; set; }
     }
 }
